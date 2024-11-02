f382a835c48e35b3b2e0761ac7084d32f3451365
ranabismah <ranabismah178@gmail.com>
2024-11-02 13:40:12 -0700

    some changing on MobileNav.jsx file.

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -7,43 +7,50 @@ import { CiMenuFries } from "react-icons/ci";
 
 const links = [
   { name: "home", path: "/" },
-  { name: "services", path: "/services" },
-  { name: "resume", path: "/resume" },
-  { name: "work", path: "/work" },
+
+  { name: "about", path: "/about" },
+  { name: "skills", path: "/skills" },
   { name: "contact", path: "/contact" },
 ];
- 
-const MobileNav =() => {
-    const pathname = usePathname();
-    return(
-        <Sheet>
-            <SheetTrigger className="flex justify-centre items-center">
-                <CiMenuFries className="text-[32px] text-blue-100"/>
-            </SheetTrigger>
-            <SheetContent className="flex flex-col">
-                {/* logo */}
-                <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link href="/">
-                    <h1 className="text-4xl font-semibold">RANA BISMA<span className="text-red-800">.</span></h1>
-                    </Link>
-                </div>
-            {/* NAV */}
 
-            <nav className="flex flex-col justify-center items-center gap-8">
-                {links.map((link, index) => {
-                    return(
-                        <Link href={link.path} key={index}
-                        className={`${
-                            link.path === pathname && "text-red-800 border-b-2 border-red-800"}
-                           text-xl capitalize  hover:text-red-700 transition-all`}>
-                            {link.name}
-                        </Link>
-                    );
-                })}
-            </nav>
-            </SheetContent>
-        </Sheet>
-    )
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-centre items-center">
+        <CiMenuFries className="text-[32px] text-blue-100" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              RANA BISMA<span className="text-red-800">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* NAV */}
+
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-red-800 border-b-2 border-red-800"
+                }
+                           text-xl capitalize  hover:text-red-700 transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
